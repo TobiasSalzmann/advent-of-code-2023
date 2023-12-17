4248e9d11687b1442aa9974fa82e4dd7796bc82f
Tobias Salzmann <tobias.salzmann@gropyus.com>
2023-12-17 02:51:00 +0100

    improve perf of day 14

--- src/day14.rs
+++ src/day14.rs
@@ -1,19 +1,19 @@
-use std::collections::{HashMap, HashSet};
-
 use crate::day14::Rock::{Fixed, Round};
 use crate::util::{AdventHelper, Point};
 use itertools::Itertools;
+use rustc_hash::FxHashMap;
+use std::collections::{HashMap, HashSet};
 
 pub fn main() {
     let advent = AdventHelper::from_file_name(file!());
     let lines = advent.parse_from_strings();
-    let grid: HashMap<Point, Rock> = grid(&lines);
+    let grid: FxHashMap<Point, Rock> = grid(&lines);
 
     advent.part1("load:  {}", load(&grid, lines.len() as i32));
     advent.part2("load:  {}", load2(&grid, lines.len() as i32, 1_000_000_000));
 }
 
-fn load(grid: &HashMap<Point, Rock>, n: i32) -> i32 {
+fn load(grid: &FxHashMap<Point, Rock>, n: i32) -> i32 {
     let mut grid = grid.clone();
 
     step(&mut grid, Point::up);
@@ -23,10 +23,10 @@ fn load(grid: &HashMap<Point, Rock>, n: i32) -> i32 {
         .sum()
 }
 
-fn load2(grid: &HashMap<Point, Rock>, n: i32, target: i32) -> i32 {
+fn load2(grid: &FxHashMap<Point, Rock>, n: i32, target: i32) -> i32 {
     let mut grid = grid.clone();
-    let mut seen: HashMap<String, i32> = HashMap::new();
-    let mut loads: HashMap<i32, i32> = HashMap::new();
+    let mut seen: FxHashMap<String, i32> = FxHashMap::default();
+    let mut loads: FxHashMap<i32, i32> = FxHashMap::default();
 
     for i in 1.. {
         step(&mut grid, Point::up);
@@ -56,7 +56,7 @@ fn load2(grid: &HashMap<Point, Rock>, n: i32, target: i32) -> i32 {
     unreachable!()
 }
 
-fn step(grid: &mut HashMap<Point, Rock>, mv: fn(&Point) -> Point) {
+fn step(grid: &mut FxHashMap<Point, Rock>, mv: fn(&Point) -> Point) {
     let col: HashSet<Point> = grid.keys().cloned().collect();
     let bounds = Point::bounds(&col);
     let mut changed = 1;
@@ -69,10 +69,17 @@ fn step(grid: &mut HashMap<Point, Rock>, mv: fn(&Point) -> Point) {
             .cloned()
             .collect_vec();
         for p in round_rocks {
-            let next = mv(&p);
-            if !grid.contains_key(&next) && bounds.contains(&next) {
+            let mut good = p;
+            loop {
+                let next = mv(&good);
+                if grid.contains_key(&next) || !bounds.contains(&next) {
+                    break;
+                }
+                good = next;
+            }
+            if good != p {
                 grid.remove(&p);
-                grid.insert(next, Round);
+                grid.insert(good, Round);
                 changed += 1;
             }
         }
@@ -85,8 +92,10 @@ enum Rock {
     Fixed,
 }
 
-fn grid(lines: &Vec<String>) -> HashMap<Point, Rock> {
-    let mut grid = HashMap::new();
+fn grid(lines: &Vec<String>) -> FxHashMap<Point, Rock> {
+    let capacity = lines.len() * lines[0].len();
+    let mut grid = FxHashMap::with_capacity_and_hasher(capacity, Default::default());
+
     for (y, line) in lines.iter().enumerate() {
         for (x, char) in line.chars().enumerate() {
             if char != '.' {
