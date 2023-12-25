5e3612e9020e96c46ce6f499c2b14c639ab0523e
Tobias Salzmann <tobias.salzmann@gropyus.com>
2023-12-25 23:50:17 +0100

    generify day24

--- src/day24.rs
+++ src/day24.rs
@@ -1,6 +1,12 @@
 use crate::util::AdventHelper;
+use gomez::algo::trust_region::TrustRegionOptions;
+use gomez::algo::{Lipo, NelderMead, TrustRegion};
+use gomez::{nalgebra as na, OptimizerDriver};
+use gomez::{Domain, Problem, SolverDriver, System};
 use itertools::Itertools;
-use num::signum;
+use na::{Dyn, IsContiguous};
+use num::{signum, Num};
+use plotters::prelude::*;
 use std::ops::Add;
 use std::str::FromStr;
 
@@ -45,27 +51,44 @@ fn intersecting_paths(hailstones: &Vec<Hailstone>, lower_bound: f64, upper_bound
     }
     count
 }
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Hailstone {
-    position: Vec3,
-    velocity: Vec3,
+    position: Vec3<i128>,
+    velocity: Vec3<i128>,
 }
 
 #[derive(Copy, Clone, Debug)]
-struct Vec3 {
-    x: i128,
-    y: i128,
-    z: i128,
+struct Vec3<T> {
+    x: T,
+    y: T,
+    z: T,
 }
 
-impl Add for Vec3 {
-    type Output = Vec3;
+impl<T: Add> Add for Vec3<T> {
+    type Output = Vec3<T::Output>;
 
     fn add(self, rhs: Self) -> Self::Output {
         Vec3 {
             x: self.x + rhs.x,
             y: self.y + rhs.y,
             z: self.z + rhs.z,
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+struct Vec2 {
+    x: f64,
+    y: f64,
+}
+
+impl Add for Vec2 {
+    type Output = Vec2;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Vec2 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
         }
     }
 }
@@ -88,4 +111,27 @@ impl FromStr for Hailstone {
             .unwrap();
         Ok(Hailstone { position, velocity })
     }
+}
+
+fn projected_intersections(hailstones: &Vec<Hailstone>) -> Vec<Vec2> {
+    let mut intersections = vec![];
+    for (a, b) in hailstones.iter().tuple_combinations() {
+        let Vec3 { x: x1, y: y1, z: _ } = a.position;
+        let Vec3 { x: x2, y: y2, z: _ } = a.position + a.velocity;
+        let Vec3 { x: x3, y: y3, z: _ } = b.position;
+        let Vec3 { x: x4, y: y4, z: _ } = b.position + b.velocity;
+
+        let denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+        let x_num = (x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4);
+        let y_num = (x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4);
+
+        if denom == 0 {
+            continue;
+        }
+        let x = x_num as f64 / denom as f64;
+        let y = y_num as f64 / denom as f64;
+
+        intersections.push(Vec2 { x, y })
+    }
+    intersections
 }
